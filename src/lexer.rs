15ca3578d50765050f236e42779ab91d4e948c8b
Sander in 't Veld <sanderintveld@hotmail.com>
2020-09-26 16:58:00 +0200

    Started working on better lexer errors with line numbers, but all tokens need to be annotated.

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,6 +1,6 @@
 /**/
 
-use anyhow::anyhow;
+use thiserror::Error;
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Token
@@ -12,6 +12,8 @@ pub enum Token
 	BraceRight,
 	Plus,
 	Minus,
+	Times,
+	Divide,
 	Colon,
 	Semicolon,
 	Assignment, // =
@@ -33,12 +35,59 @@ pub enum Token
 	Bool(bool),
 }
 
-pub fn lex(source: &str) -> Result<Vec<Token>, anyhow::Error>
+#[derive(Debug, Error)]
+pub enum Error
+{
+	#[error("unexpected character {character:?}")]
+	UnexpectedCharacter
+	{
+		character: char
+	},
+	#[error("invalid integer")]
+	InvalidInteger(#[from] std::num::ParseIntError),
+}
+
+impl Error
+{
+	fn at(self, line: usize, line_offset: usize) -> AnnotatedError
+	{
+		AnnotatedError {
+			error: self,
+			line,
+			line_offset,
+		}
+	}
+}
+
+#[derive(Debug)]
+pub struct AnnotatedError
+{
+	error: Error,
+	line: usize,
+	line_offset: usize,
+}
+
+impl std::error::Error for AnnotatedError {}
+impl std::fmt::Display for AnnotatedError
+{
+	fn fmt(
+		&self,
+		f: &mut std::fmt::Formatter,
+	) -> std::result::Result<(), std::fmt::Error>
+	{
+		write!(f, "at {}:{}: {}", self.line, self.line_offset, self.error)
+	}
+}
+
+pub fn lex(source: &str) -> Result<Vec<Token>, AnnotatedError>
 {
 	let mut tokens = Vec::new();
-	let mut iter = source.chars().peekable();
-	while let Some(x) = iter.next()
+	let mut line = 0;
+	let mut last_line_start_offset = 0;
+	let mut iter = source.chars().enumerate().peekable();
+	while let Some((offset_of_x, x)) = iter.next()
 	{
+		let line_offset = offset_of_x - last_line_start_offset;
 		let token = match x
 		{
 			'(' => Token::ParenLeft,
@@ -47,11 +96,12 @@ pub fn lex(source: &str) -> Result<Vec<Token>, anyhow::Error>
 			'}' => Token::BraceRight,
 			'+' => Token::Plus,
 			'-' => Token::Minus,
+			'*' => Token::Times,
 			':' => Token::Colon,
 			';' => Token::Semicolon,
 			'=' => match iter.peek()
 			{
-				Some('=') =>
+				Some((_, '=')) =>
 				{
 					iter.next();
 					Token::Equals
@@ -60,30 +110,25 @@ pub fn lex(source: &str) -> Result<Vec<Token>, anyhow::Error>
 			},
 			'/' => match iter.peek()
 			{
-				Some('/') =>
+				Some((_, '/')) =>
 				{
-					while let Some(y) = iter.next()
+					while let Some((offset_of_y, y)) = iter.next()
 					{
 						if y == '\n'
 						{
+							line += 1;
+							last_line_start_offset = offset_of_y + 1;
 							break;
 						}
 					}
 					continue;
 				}
-				Some(y) =>
-				{
-					return Err(anyhow!("unexpected sequence '{}' '{}'", x, y));
-				}
-				None =>
-				{
-					return Err(anyhow!("unexpected final character '{}'", x));
-				}
+				_ => Token::Divide,
 			},
 			'a'..='z' | 'A'..='Z' | '_' =>
 			{
 				let mut identifier = x.to_string();
-				while let Some(&y) = iter.peek()
+				while let Some(&(_, y)) = iter.peek()
 				{
 					if is_identifier_continuation(y)
 					{
@@ -111,7 +156,7 @@ pub fn lex(source: &str) -> Result<Vec<Token>, anyhow::Error>
 			'0'..='9' =>
 			{
 				let mut literal = x.to_string();
-				while let Some(&y) = iter.peek()
+				while let Some(&(_, y)) = iter.peek()
 				{
 					if y.is_digit(10)
 					{
@@ -123,11 +168,32 @@ pub fn lex(source: &str) -> Result<Vec<Token>, anyhow::Error>
 						break;
 					}
 				}
-				let value = literal.parse()?;
+				let value: i32 = match literal.parse()
+				{
+					Ok(value) => value,
+					Err(error) =>
+					{
+						return Err(AnnotatedError {
+							error: error.into(),
+							line,
+							line_offset,
+						})
+					}
+				};
 				Token::Int32(value)
 			}
-			' ' | '\t' | '\r' | '\n' => continue,
-			_ => return Err(anyhow!("unexpected character '{}'", x)),
+			' ' | '\t' | '\r' => continue,
+			'\n' =>
+			{
+				line += 1;
+				last_line_start_offset = offset_of_x + 1;
+				continue;
+			}
+			_ =>
+			{
+				return Err(Error::UnexpectedCharacter { character: x }
+					.at(line, line_offset))
+			}
 		};
 		tokens.push(token);
 	}
