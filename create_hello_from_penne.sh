a22900334e984ae39308629c3f155263589f9fba
Sander in 't Veld <sanderintveld@hotmail.com>
2022-09-23 01:27:23 +0200

    Trying to determine why the wasm-ld --import-memory does not do what I need.

--- create_hello_from_penne.sh
+++ create_hello_from_penne.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 /opt/wasi-sdk/bin/clang -nostartfiles -nostdlib \
-	-Wl,--no-entry -Wl,--allow-undefined -Wl,--export=update \
+	-Wl,--allow-undefined -Wl,--import-memory \
+	-Wl,--no-entry -Wl,--export=update \
 	bin/src/samples/wasm4_hello_from_penne.pn.ll \
 	--target=wasm32-unknown-wasi \
 	-o bin/hello_from_penne.wasm
