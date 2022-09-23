412e3f21aad3c1ef5efce3107b8cdf1caf253331
Sander in 't Veld <sanderintveld@hotmail.com>
2022-09-23 11:06:09 +0200

    Needed some extra flags for wasm-ld.

--- create_hello_from_penne.sh
+++ create_hello_from_penne.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 /opt/wasi-sdk/bin/clang -nostartfiles -nostdlib \
 	-Wl,--allow-undefined -Wl,--import-memory \
+	-Wl,--initial-memory=65536 -Wl,--max-memory=65536 \
+	-Wl,-z,stack-size=14752 -Wl,--stack-first \
 	-Wl,--no-entry -Wl,--export=update \
 	bin/src/samples/wasm4_hello_from_penne.pn.ll \
-	--target=wasm32-unknown-wasi \
+	--target=wasm32-unknown-unknown \
 	-o bin/hello_from_penne.wasm
